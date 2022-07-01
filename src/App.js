94a2da31b6e20e4257179c6be786c4a148e844ef
vilas <atechy.vilas@gmail.com>
2022-07-01 06:58:57 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Bootstrap
+          Learn React and material ui
         </a>
       </header>
     </div>
